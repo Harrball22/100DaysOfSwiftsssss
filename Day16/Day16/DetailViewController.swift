4c32a9588d4964cc2ed2b0d8b2a9f0810ba4c885
Harrison Dutson <totodutson@gmail.com>
2020-09-18 11:18:11 -0600

    Day 18
     - Table cells are now auto sorted.
     - Table cell text size changed from 15 to 22.
     - The title bar in DetailedViewController now displays (image number out of total images.

--- Day16/Day16/DetailViewController.swift
+++ Day16/Day16/DetailViewController.swift
@@ -9,14 +9,30 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    var imageNumber: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = imageNumber
+        navigationItem.largeTitleDisplayMode = .never
 
-        // Do any additional setup after loading the view.
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
     /*
     // MARK: - Navigation
 
