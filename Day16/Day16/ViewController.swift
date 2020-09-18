88a7aafbd1d795d23ba7ba14af22b0db25197a9d
Harrison Dutson <totodutson@gmail.com>
2020-09-18 09:50:14 -0600

    Day 16 + Day 17
    Accidentally forgot to commit Day 16 before doing Day 17

---
+++ Day16/Day16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day16
+//
+//  Created by Harrison Dutson on 9/15/20.
+//  Copyright © 2020 Harrison Dutson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
